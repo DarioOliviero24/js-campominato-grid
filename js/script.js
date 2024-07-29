bbd526f72eda3400bcafac317b890ad859098e30
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-29 18:15:15 +0200

    C'E L'HO FATTA

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-const start = document.getElementById("start");
-const section = document.querySelector(".section");
+const square = parseInt( prompt('inserisci il numero'));
+
+console.log('square',square, typeof square);
+
+const container = document.getElementById('container');
+
+
+for (let i = 0; i < square; i++){
+    const newcell = document.createElement('div');
+    newcell.innerHTML = (i + 1);
+    
+    newcell.addEventListener('click',function () {
+    newcell.classList.toggle('dario');
+
+    console.log(newcell.innerHTML);
+    
+});
+
+container.append(newcell);
+}
